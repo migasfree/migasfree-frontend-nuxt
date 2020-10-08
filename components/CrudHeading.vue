3f8f78c085e0bd6a2a95d2114d01459dcecdd99e
Jose Antonio Chavarría <jachavar@gmail.com>
2020-10-08 10:29:52 +0200

    If has not add button, improved visualization

--- components/CrudHeading.vue
+++ components/CrudHeading.vue
@@ -1,11 +1,11 @@
 <template>
   <v-row>
-    <v-col cols="1">
-      <v-btn v-if="hasAddButton" fab dark color="primary">
+    <v-col v-if="hasAddButton" cols="1">
+      <v-btn fab dark color="primary">
         <v-icon dark>mdi-plus</v-icon>
       </v-btn>
     </v-col>
-    <v-col cols="11">
+    <v-col :cols="titleCols">
       <h2 class="display-2">
         {{ title }} <v-chip>{{ results }}</v-chip>
       </h2>
@@ -19,6 +19,11 @@ export default {
     title: { type: String, required: true },
     results: { type: Number, required: false, default: 0 },
     hasAddButton: { type: Boolean, required: false, default: true },
+  },
+  computed: {
+    titleCols() {
+      return this.hasAddButton ? 11 : 12
+    },
   },
 }
 </script>
